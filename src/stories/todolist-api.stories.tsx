03d61ccecf103af5a9cfea8a2f1a744bc22f3ea8
madyler <madyler@mail.ru>
2022-03-03 23:54:59 +0300

    api-stories

---
+++ src/stories/todolist-api.stories.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+
+export default {
+    title: 'API'
+}
+
+const settings = {
+    withCredentials: true,
+    headers: {
+        'API-KEY': '00adb810-fa6f-4427-8bcb-00218cb88b97'
+    }
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        // здесь мы будем делать запрос и ответ закидывать в стейт.
+        // который в виде строки будем отображать в div-ке
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -5,12 +5,19 @@ export default {
     title: 'API'
 }
 
+const settings = {
+    withCredentials: true,
+    headers: {
+        'API-KEY': '00adb810-fa6f-4427-8bcb-00218cb88b97'
+    }
+}
+
 export const GetTodoLists = () => {
 
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists')
+        axios.get('https://social-network.samuraijs.com/api/1.1/auth/me', settings)
             .then((res) => {
                 debugger
             })
